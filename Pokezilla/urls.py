466cccf07877b2e4d6aba37b41431a8da9003678
CinquinAndy <cinquin.andy@gmail.com>
2022-01-01 12:47:59 +0100

    specific pokemon

--- Pokezilla/urls.py
+++ Pokezilla/urls.py
@@ -19,5 +19,6 @@ from Pokezilla import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:number>', views.pokemon, name="pokemon")
 ]
