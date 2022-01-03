e9ee7ae2f842f17dd5a17919bfe3dcb2a50e73a1
Breval Le Floch <breval2000@live.fr>
2022-01-03 11:28:55 +0100

    link and search bar value

--- static/pokedex/script.js
+++ static/pokedex/script.js
@@ -1,3 +1,26 @@
+let cards
+
+
 window.onload = function () {
+
     window.scrollTo(0, 300);
+
+    cards = document.querySelectorAll(".card")
+    console.log(cards)
+
+}
+
+
+function search(input) {
+    console.log("text : " + input)
+
+    for (e in cards) {
+
+        if (e.  includes(input)) {
+            console.log(e)
+
+
+        }
+    }
+
 }
