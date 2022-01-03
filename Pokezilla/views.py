f01b2e71202442ebe3ff8b71cb4d38317b0f2dd5
Breval Le Floch <breval2000@live.fr>
2022-01-03 12:53:49 +0100

    filter + page index + page solo

--- Pokezilla/views.py
+++ Pokezilla/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
-
-def index(request):
-    # ici
-    contextvalue = {"valeur": "valeurTest", "table": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-    return render(request, 'index.html', contextvalue)
+from Pokezilla.request.get__all_pokemon import get_all_pokemon
+from Pokezilla.request.get_pokemon_infos import get_pokemon
 
 
-def pokemon(request, number):
-    print(number)
-    return render(request, 'pokemon.html', {"number": number})
+async def index(request):
+	# ici
+	data = await get_all_pokemon()
+	contextvalue = {"values": data}
+
+	return render(request, 'index.html', contextvalue)
+
+
+async def pokemon(request, number):
+	print(number)
+	data = await get_pokemon(number)
+	return render(request, 'pokemon.html', {"pokemon": data,"number": number})
--- static/pokedex/script.js
+++ static/pokedex/script.js
@@ -8,19 +8,28 @@ window.onload = function () {
     cards = document.querySelectorAll(".card")
     console.log(cards)
 
+
 }
 
 
 function search(input) {
     console.log("text : " + input)
 
-    for (e in cards) {
+    var i;
+    for (i = 0; i < cards.length; i++) {
 
-        if (e.  includes(input)) {
-            console.log(e)
+        if (cards[i].innerHTML.includes(input)) {
+            console.log(cards[i]);
 
 
         }
+        else {
+            cards[i].classList.add('hidden');
+        }
+
+
+
     }
+
 
 }
