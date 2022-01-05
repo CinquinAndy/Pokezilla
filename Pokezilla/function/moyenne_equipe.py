1f82ba4fc6713b1e3fec3ebc30122e8dd4ba75b0
Mathis <math44100@yahoo.fr>
2022-01-05 15:00:48 +0100

    calcul des stats moyennes de l'équipe de pokémons sélectionnés

--- Pokezilla/function/moyenne_equipe.py
+++ Pokezilla/function/moyenne_equipe.py
@@ -1,8 +1,45 @@
-from Pokezilla.request.get_pokemon_infos import get_pokemon
+from Pokezilla.request.get_pokemon_infos import *
 
 
-def moyenne(pokemons):
-    result = []
+async def average(pokemons):
+    calculs = []
     for pokemon in pokemons:
-        result += await get_pokemon(pokemon)
-    print(result)
+        data = await get_pokemon(pokemon)
+        calculs.append(data['stats'])
+    averageHp = 0
+    averageAttack = 0
+    averageDefense = 0
+    averageAttackSpe = 0
+    averageDefenseSpe = 0
+    averageSpeed = 0
+    for calcul in calculs:
+        for stat in calcul:
+            if stat['stat']['name'] == "hp":
+                averageHp += stat['base_stat']
+            if stat['stat']['name'] == "attack":
+                averageAttack += stat['base_stat']
+            if stat['stat']['name'] == "defense":
+                averageDefense += stat['base_stat']
+            if stat['stat']['name'] == "special-attack":
+                averageAttackSpe += stat['base_stat']
+            if stat['stat']['name'] == "special-defense":
+                averageDefenseSpe += stat['base_stat']
+            if stat['stat']['name'] == "speed":
+                averageSpeed += stat['base_stat']
+
+    averageHp /= len(pokemons)
+    averageAttack /= len(pokemons)
+    averageDefense /= len(pokemons)
+    averageAttackSpe /= len(pokemons)
+    averageDefenseSpe /= len(pokemons)
+    averageSpeed /= len(pokemons)
+
+    result = {
+        "averageHp": averageHp,
+        "averageAttack": averageAttack,
+        "averageDefense": averageDefense,
+        "averageAttackSpe": averageAttackSpe,
+        "averageDefenseSpe": averageDefenseSpe,
+        "averageSpeed": averageSpeed
+    }
+    return result
