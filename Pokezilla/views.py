a3177b7267dd1c1c74dbb8533b55768884d0dd7f
Breval Le Floch <breval2000@live.fr>
2022-01-05 17:55:06 +0100

    Weakness

--- Pokezilla/views.py
+++ Pokezilla/views.py
@@ -1,27 +1,44 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from Pokezilla.function.type_transfert import get_weakness
 from Pokezilla.request.get__all_pokemon import get_all_pokemon, get_names_pokemons
 from Pokezilla.request.get_pokemon_infos import get_pokemon
 
 
 def testcall(request):
-    text = request.POST.getlist('text[]')
-    # Do whatever with the input variable text
-    response = text
-    # Send the response
+	text = request.POST.getlist('text[]')
+	# Do whatever with the input variable text
+	response = text
+	# Send the response
 
-    return HttpResponse(response)
+	return HttpResponse(response)
 
 
 async def index(request):
-    data = await get_all_pokemon()
-    contextvalue = {"values": data}
+	data = await get_all_pokemon()
+	contextvalue = {"values": data}
 
-    return render(request, 'index.html', contextvalue)
+	return render(request, 'index.html', contextvalue)
 
 
 async def pokemon(request, number):
-    print(number)
-    data = await get_pokemon(number)
-    return render(request, 'pokemon.html', {"pokemon": data, "number": number})
+	data = await get_pokemon(number)
+
+	types = []
+	weakness = set()
+
+	for t in data["types"]:
+		types.append(t["type"]["name"])
+
+	for type in types:
+		print(type)
+
+		weaks = get_weakness(type)
+		print(weaks)
+		for w in weaks:
+			print(w)
+			weakness.add(w)
+	weakness = list(weakness)[:6]
+
+	return render(request, 'pokemon.html', {"pokemon": data, "number": number, "weakness": weakness})
